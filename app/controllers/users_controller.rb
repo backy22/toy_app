d567fdfe78e9bb0869da5f0e8416576991129bed
椿野彩 <tsubakino@logiclogic.jp>
2018-10-15 04:56:29 +0900

    fix micropost_content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_first_micropost, only: [:show, :edit]
 
   # GET /users
   # GET /users.json
@@ -65,6 +66,10 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def set_first_micropost
+      @micropost_content = @user.microposts.first.content rescue nil
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
